2d5c73468c806485d3d60abff6bac22140498f51
willschuerman <wschuerm@gmail.com>
2020-09-03 13:32:45 -0700

    perfected thresholds and paramters for two speakers

--- Functions/Tracker/trackPitch.m
+++ Functions/Tracker/trackPitch.m
@@ -1,7 +1,7 @@
-function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims)
+function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims,amp_mod)
     %%
     myLines = [];
-    [~,fs] = audioread(fname);
+    [s,fs] = audioread(fname);
     tone = str2double(fname(end-7));
     ncandidates = 5;
     
@@ -10,11 +10,12 @@ function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims)
     spf = ceil(tstep*fs); % will need to edit this
     afr = dsp.AudioFileReader('Filename',fname,'PlayCount',1,'SamplesPerFrame',spf);
     adw = audioDeviceWriter('SampleRate', afr.SampleRate);
-    
+    nframes = ceil(length(s)/spf);
     % get first audio frame (which we do not store)
     frame = afr();
+    [f0_cand,pdc_cand] = getCandidateF0(frame,fs,pitch_lims,ncandidates);
     amp_tm1 = max(frame); 
-    t(1) = 0;
+    t(1) = -tstep;
     
     pdc_tm1 = NaN;
     f0_tm1 = NaN;
@@ -22,18 +23,17 @@ function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims)
     f0s(1) = NaN;
     pdcs(1) = NaN;
 
+    amp_threshold = amp_tm1*amp_mod; % set threshold for sound
     if tone == 3
         pitch_lims(1) = ceil(1/tstep)+1;
+        pdc_threshold = 0;
         amp_threshold = 0;
-    else
-        amp_threshold = amp_tm1*0.5; % set threshold for sound
-
     end
     
     
     % get subsequent audio frames
     cnt = 2;
-    while ~isDone(afr)
+    while ~isDone(afr) && cnt < nframes-2
         frame = afr();
         amp_t = max(frame); 
         t(cnt) = t(cnt-1)+tstep;
@@ -46,47 +46,21 @@ function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims)
             elseif isempty(f0_cand) % if the current value is nan
                 pdc_t = NaN;
                 f0_t = NaN;
-            elseif isnan(f0_tm1) % the previous value is nan
-                f0_t = f0_cand(1);
-                pdc_t = pdc_cand(1); % easy for first sample... unless nan...
-            else % actually extract f0
-                % modify the probabilities of a pitch by 1) distance from previous
-                % pitch, 2) tone type (i.e., we should penalize
-                % decreases for tone 2 and increases for tone 4, and not penalize
-                % very low pitches for tone, and 3) periodicity a.k.a.
-                % confidence in measurement (literally xcorr peak height).
-                
-                % 1) get difference betweeen current and previous candidates (need
-                % to fix so that matrices are always same size
-                f0diff = f0_cand-f0_tm1;
-                
-                % 2) modulate differences depending on tone type
-                [f0diff] = modProbs(f0diff,tone); 
-                
-                % 3) scale by strength of periodicity
-                f0diff = f0diff.*(1-pdc_cand);
-                
-                % 4) select tracked pitch
-                [~,idx]= min(f0diff);
-                f0_t = f0_cand(idx);
-                pdc_t = pdc_cand(idx);
-
+            else
+                f0_t = f0_cand(1); % using best candidate gives best results
+                pdc_t = pdc_cand(1);
             end
         else
             pdc_t = NaN;
             f0_t = NaN; 
         end
         
-        % smooth values and extrapolate NaNs
-        x = t([cnt-1, cnt]);
-        y = [f0_tm1 f0_t];
-        y = fillmissing(y','pchip','EndValues','extrap');
-        
-        
-        hold on
-        myLines(1) = plot(x,y,'b','linewidth',2);
+        f0_t = fillmissing([f0s f0_t]','pchip','EndValues','extrap');
+        f0_t = f0_t(end);
+        % extrapolate NaNs fi they exist
+        x = t([cnt-1, cnt])';
+        y = [f0_tm1 f0_t]';        
 
-        
         % store
         f0s(cnt) = f0_t;
         amps(cnt) = amp_t;
@@ -101,8 +75,8 @@ function [f0s,t,amps,pdcs] = trackPitch(fname,pitch_lims)
         cnt = cnt+1;
         
         % plot when hits right time stamp
-        drawnow  
-        WaitSecs(tstep/2);
+        %drawnow  
+        %WaitSecs(tstep/2);
     end
     
     release(afr);
--- Scripts/scratch_08172020.m
+++ Scripts/scratch_08172020.m
@@ -4,21 +4,24 @@ addpath(genpath('/Users/willschuerman/Documents/Research/Tasks/realtimepitch_mat
 %% Basic pitch tracking
 stimdir = '/Users/willschuerman/Documents/Research/Tasks/realtimepitch_matlab/files/stimuli';
 
-files = dir(fullfile(stimdir,'*hN.wav'));
+files = dir(fullfile(stimdir,'*aN.wav'));
 num_files = size(files,1);
 
-%% Use PRAAT as gold standard
+%% Use PRAAT as gold standard, compare with pitch tracking
 close all
 for sf = 1:20
     subplot(4,5,sf)
     fname = [stimdir filesep files(sf).name];
     speaker_id = fname(end-5);
-    if any(strcmp(speaker_id,{'a','b'})) % WORKS BEST FOR A
-        gender = 'm';
-        pitch_lims = [70 300];
-    else % h,i WORKS BEST FOR I
-        gender = 'f';
-        pitch_lims = [80 400];
+    switch speaker_id % WORKS BEST FOR A and I
+        case 'a' 
+            gender = 'm';
+            pitch_lims = [70 220];
+            amp_mod = 1.1;   
+        case 'i' 
+            gender = 'f';
+            pitch_lims = [80 350];
+            amp_mod = 0.1;            
     end
     [f0_praat, t_praat] = extractPitchWithPraat(fname);
     plot(t_praat,f0_praat,'r','linewidth',2);
@@ -29,8 +32,10 @@ for sf = 1:20
 %     soundsc(s,fs)
     % Testing incremental tracker against PRAAT
 
-    [f0s, fts, amps,pdcs] = trackPitch(fname,pitch_lims);
+    [f0s, fts, amps,pdcs] = trackPitch(fname,pitch_lims,amp_mod);
+    plot(fts,f0s,'b','linewidth',2);
     title(files(sf).name)
 end
-%% Test live pitch tracking against both
 
+
+%%
